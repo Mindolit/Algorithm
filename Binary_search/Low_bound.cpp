32ebede9bee227e25d7bc2452acc60cc80c4011f
Mindolit <kms5486@hanmail.net>
2022-02-09 17:35:24 +0900

    .

---
+++ Binary_search/Low_bound.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Mindolit_low_bound_binary_search(int *arr, int target, int start ,int end){
+  while(start<end)
+  { 
+    int mid=(start+end)/2; 
+    if(arr[mid]<target){
+      start=mid+1;
+    }
+    else if(arr[mid]>=target){
+      end=mid;
+    }
+  }
+  return end;
+}
+
+int main(){
+  int array[11]={1,1,1,2,2,2,3,3,3,4,6};
+  int target;
+  cin>>target;
+  cout<<Mindolit_low_bound_binary_search(array,target,0,sizeof(array)/sizeof(int)-1);
+  system("pause");
+}
