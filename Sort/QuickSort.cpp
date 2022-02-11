2aaadf87b767f8c666a38da5c82377032a571436
Mindolit <kms5486@hanmail.net>
2022-02-11 14:44:53 +0900

    퀵소트

--- Sort/QuickSort.cpp
+++ Sort/QuickSort.cpp
@@ -1,9 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void QuickSort();
+void QuickSort(int [],int,int);
 
 int main(){
-  int unsorted_arr[10]={10,9,2,4,3,1,11,16,0,-1};
-  QuickSort();
+  int unsorted_arr[9]={5,3,8,4,9,1,6,2,7};
+  QuickSort(unsorted_arr,0,9);
+  for(int i:unsorted_arr){
+    cout<<i<<" ";
+  }
+  system("pause");
+}
+
+void QuickSort(int arr[],int start,int end){
+  int pivot=arr[start];
+  int low=start+1;
+  int high=end;
+  while(low<=high){
+    while(arr[low]<=pivot && low<=end){
+      low++;
+    }
+
+    while(arr[high]>=pivot && start+1<=high){
+      high--;
+    }
+    
+    if(low<=high){
+      int tmp=arr[low];
+      arr[low]=arr[high];
+      arr[high]=tmp;
+    }
+  }
+  if(start<end){
+  int tmp=arr[high];
+  arr[high]=pivot;
+  arr[start]=tmp;
+  QuickSort(arr,start,high-1);
+  QuickSort(arr,high+1,end);
+  }
 }
