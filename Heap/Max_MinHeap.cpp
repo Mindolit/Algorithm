45ca704a24559ea05e5409c06d925d5b889738ea
Mindolit <kms5486@hanmail.net>
2022-02-14 15:01:43 +0900

    add MAX_HEAP_POP()

--- Heap/Max_MinHeap.cpp
+++ Heap/Max_MinHeap.cpp
@@ -33,8 +33,37 @@ public:
     }
   }
 
+  void max_heap_pop(){
+    cout<<"---------MAX_HEAP_POP----------"<<'\n';
+    heap[1]=heap[heap_idx-1];
+    int tmp=heap[1];
+    heap_idx--;
+    int parent=1, child=2;
+    while(child<=heap_idx){
+      if(heap[child]>=tmp || heap[child+1]>=tmp){
+        if(heap[child]>heap[child+1]){
+          swap(heap[child],heap[parent]);
+        }
+        else{
+          child=child+1;
+          swap(heap[child],heap[parent]);
+        }
+        parent=child;
+        child=child*2;
+      }
+      else{ break; }
+
+
+    }
+
+
+  }
+
+  void min_heap_pop(){
+
+  }
   void print_top(){
-    cout<<"MAX_HEAL_TOP:"<<heap[1]<<'\n';
+    cout<<"MAX_HEAP_TOP:"<<heap[1]<<'\n';
     cout<<"MIN_HEAP_TOP:"<<min_heap[1]<<'\n';
   }
 };
@@ -46,6 +75,8 @@ int main(){
   a.push(1);
   a.push(11);
   a.push(13);
+  a.print_top();
+  a.max_heap_pop();
   a.print_top();
   system("pause");
 }
